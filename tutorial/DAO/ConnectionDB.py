90e164e40d7af5b62569a4f2a4753a0b9fb06991
rafael <rafaelxld@hotmail.com>
2019-12-28 11:54:03 -0300

    corrigindo formato de insercao

--- tutorial/DAO/ConnectionDB.py
+++ tutorial/DAO/ConnectionDB.py
@@ -10,7 +10,7 @@ class ConnectionDB(object):
 
     def __init__(self):
       self.connection = self.getConnection()
-      self.solr = pysolr.Solr('http://localhost:8983/solr/mycol1/', timeout=10)
+      self.solr = pysolr.Solr('http://192.168.1.7:8983/solr/links/', timeout=10)
 
 
 
