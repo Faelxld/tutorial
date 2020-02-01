56bc97933f2fcb5411b09b6985f449134136f8f7
rafael <rafaelxld@hotmail.com>
2020-02-01 20:14:43 -0300

    logger add

--- tutorial/DAO/ConnectionDB.py
+++ tutorial/DAO/ConnectionDB.py
@@ -10,7 +10,7 @@ class ConnectionDB(object):
 
     def __init__(self):
       self.connection = self.getConnection()
-      self.solr = pysolr.Solr('http://192.168.1.7:8983/solr/links/', timeout=10)
+      self.solr = pysolr.Solr('http://servermon.ddns.net:8983/solr/', timeout=10)
 
 
 
